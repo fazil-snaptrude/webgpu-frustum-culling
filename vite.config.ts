7999ecb368674314c82384af01ad861df10eb0a5
Fazil Babu <fazil@snaptrude.com>
2025-10-08 10:15:03 +0530

    chore: Deploy to GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { linkBuildExtension } from "wesl-plugin";
 import viteWesl from "wesl-plugin/vite";
 
 export default {
+  base: process.env.CI ? "/webgpu-frustum-culling/" : "/",
   plugins: [viteWesl({ extensions: [linkBuildExtension] })],
 };
